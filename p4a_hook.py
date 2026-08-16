d565e25bd0809a67bd3a0e9f8813a9dccf33699a
egordyatloff-hue <egordyatloff@gmail.com>
2026-08-16 23:36:20 +0700

    Create p4a_hook.py

---
+++ p4a_hook.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import os
+
+
+def after_apk_build(toolchain_cl):
+    manifest = os.path.join(os.getcwd(), "src", "main", "AndroidManifest.xml")
+    if not os.path.exists(manifest):
+        return
+    with open(manifest, "r", encoding="utf-8") as f:
+        text = f.read()
+    if "org.example.toratime.AlarmReceiver" in text:
+        return
+    receiver = (
+        '<receiver android:name="org.example.toratime.AlarmReceiver" '
+        'android:enabled="true" android:exported="false">\n'
+        '        <intent-filter>\n'
+        '            <action android:name="org.example.toratime.ALARM"/>\n'
+        '        </intent-filter>\n'
+        '        <intent-filter>\n'
+        '            <action android:name="android.intent.action.BOOT_COMPLETED"/>\n'
+        '        </intent-filter>\n'
+        '    </receiver>'
+    )
+    idx = text.rfind("</application>")
+    if idx == -1:
+        return
+    text = text[:idx] + "    " + receiver + "\n" + text[idx:]
+    with open(manifest, "w", encoding="utf-8") as f:
+        f.write(text)
+    print("AlarmReceiver injected into AndroidManifest.xml")
